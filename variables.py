ae44bc681306944cae1c3d8afc486d442ce8c10d
Anghelo <anghelohuanegutierrez14@gmail.com>
2026-09-04 22:06:40 -0500

    Prueba modificaciones nueva rama

--- variables.py
+++ variables.py
@@ -6,10 +6,22 @@ dato = "Telaraña organica"
 
 print()
 
-print("====DATOS SPIDER MAN====")
+print("====DATOS DEL PRIMER SPIDER MAN====")
 print("Nombre: " + nombre)
 print("Edad: " + str(edad))
 print("Dato: " + dato)
+
+print()
+print("==========NUEVO SPIDER MAN============")
+nombre = "Andrew Garfield"
+edad = 26
+dato = "Telaraña artificial y su pareja es Gwen"
+
+print("====DATOS DEL SEGUNDO SPIDER MAN====")
+print("Nombre: " + nombre)
+print("Edad: " + str(edad))
+print("Dato: " + dato)
+
 
 print()
 print("==========NUEVO SPIDER MAN============")
@@ -17,7 +29,10 @@ nombre = "Tom Holland"
 edad = 20
 dato = "Telaraña artificial"
 
-print("====DATOS SPIDER MAN====")
+print("====DATOS DEL TERCER SPIDER MAN====")
 print("Nombre: " + nombre)
 print("Edad: " + str(edad))
 print("Dato: " + dato)
+
+print()
+print("¡Estoy aprendiendo a usar ramas en Git!")
