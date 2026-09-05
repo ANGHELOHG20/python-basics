b961e05f13da175affed7fd183115f167cdbd850
Anghelo <anghelohuanegutierrez14@gmail.com>
2026-09-04 22:01:19 -0500

    cambio de variables y .gitignore actualizado

---
+++ variables.py
@@ -0,0 +1,23 @@
+print("======SPIDER MAN=====")
+print()
+nombre = "Tobey Maguire"
+edad= 28
+dato = "Telaraña organica"
+
+print()
+
+print("====DATOS SPIDER MAN====")
+print("Nombre: " + nombre)
+print("Edad: " + str(edad))
+print("Dato: " + dato)
+
+print()
+print("==========NUEVO SPIDER MAN============")
+nombre = "Tom Holland"
+edad = 20
+dato = "Telaraña artificial"
+
+print("====DATOS SPIDER MAN====")
+print("Nombre: " + nombre)
+print("Edad: " + str(edad))
+print("Dato: " + dato)
